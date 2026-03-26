ce7f0e85430cb63ae141ada692a1a13be0fca8e8
Ryan Baker <rbaker@xtier.com>
2026-03-26 18:58:38 -0400

    Bump gitvista to 1.6.0

--- Formula/gitvista.rb
+++ Formula/gitvista.rb
@@ -1,13 +1,13 @@
 class Gitvista < Formula
   desc "Real-time local Git repository visualization"
   homepage "https://gitvista.io"
-  version "1.5.1"
+  version "1.6.0"
   license "Apache-2.0"
 
   on_macos do
       if Hardware::CPU.arm?
         url "https://github.com/rybkr/gitvista/releases/download/v#{version}/gitvista_#{version}_darwin_arm64.tar.gz"
-        sha256 "6ad55d098c559d2cf6160ffcbde10e086666239aceaa1dcd11fede5b53511fed"
+        sha256 "1e5a06c73cf11d995c9a673490ac07d65595e11bb022eb585666b18ed3417ff7"
       elsif Hardware::CPU.intel?
         url "https://github.com/rybkr/gitvista/releases/download/v#{version}/gitvista_#{version}_darwin_amd64.tar.gz"
         sha256 ""
