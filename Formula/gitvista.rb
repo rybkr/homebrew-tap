d41691c0383ca5902ad1637223c05bf73fd8ee42
Ryan Baker <rbaker@xtier.com>
2026-03-26 18:34:44 -0400

    Update install to use binary rather than build form source

--- Formula/gitvista.rb
+++ Formula/gitvista.rb
@@ -1,27 +1,36 @@
 class Gitvista < Formula
   desc "Real-time local Git repository visualization"
   homepage "https://gitvista.io"
-  url "https://github.com/rybkr/gitvista/archive/refs/tags/v1.5.1.tar.gz"
-  sha256 "cc2223980f10441ea9e5527305521df53543e9f3c4e4e19b81fded182738097e"
+  version "1.5.1"
   license "Apache-2.0"
 
-  depends_on "go" => :build
+  on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/rybkr/gitvista/releases/download/v#{version}/gitvista_#{version}_darwin_arm64.tar.gz"
+        sha256 "6d23afae8fdeac84e96df95363cf0acc730e53ce"
+      elsif Hardware::CPU.intel?
+        url "https://github.com/rybkr/gitvista/releases/download/v#{version}/gitvista_#{version}_darwin_amd64.tar.gz"
+        sha256 "9b9c35d9038f250d49319668cdb58ae8105a7e4e"
+      end
+    end
 
-  def install
-    ldflags = %W[
-      -s -w
-      -X main.version=#{version}
-    ]
+    on_linux do
+      if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        url "https://github.com/rybkr/gitvista/releases/download/v#{version}/gitvista_#{version}_linux_arm64.tar.gz"
+        sha256 "b851133b86de9b6403e609ba7375473272b54277"
+      elsif Hardware::CPU.intel?
+        url "https://github.com/rybkr/gitvista/releases/download/v#{version}/gitvista_#{version}_linux_amd64.tar.gz"
+        sha256 "1c459634b0d2a7ffdf9afea657ff3f67a30d79ec"
+      end
+    end
 
-    system "go", "build",
-        *std_go_args(ldflags: ldflags, output: bin/"gitvista"),
-        "./cmd/vista"
+    def install
+      bin.install "gitvista"
+      bin.install_symlink bin/"gitvista" => "git-vista"
+    end
 
-    bin.install_symlink bin/"gitvista" => "git-vista"
+    test do
+      assert_match "GitVista", shell_output("#{bin}/gitvista help")
+      assert_match "GitVista", shell_output("#{bin}/git-vista help")
+    end
   end
-
-  test do
-    assert_match "GitVista", shell_output("#{bin}/gitvista help")
-    assert_match "GitVista", shell_output("#{bin}/git-vista help")
-  end
-end
